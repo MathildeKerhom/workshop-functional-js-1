134d7ff80d8aff900e1e68d46cb7a7e4945cb721
Mathilde <mkerhom@gmail.com>
2017-04-21 10:17:02 +0200

    Exercice 3

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const chalk = require('chalk');
 const exercise1 = require('./src/exercise1');
 const exercise2 = require('./src/exercise2');
-// const exercise3 = require('./src/exercise3');
+const exercise3 = require('./src/exercise3');
 // const exercise4 = require('./src/exercise4');
 
 const exercise0 = {
@@ -11,9 +11,9 @@ const exercise0 = {
 };
 
 
-const exercise = exercise2;
+const exercise = exercise3;
 
 //const input = [['a', 'b'], [1], [true, false, false]];
-const input = ['033GKNORT', [0,0,1,1,2,2,4,7]];
+const input = '033GKNORT';
 
 console.log(chalk.yellow('RESULT', exercise.run(input)));
--- src/exercise3.js
+++ src/exercise3.js
@@ -1,10 +1,14 @@
 const chalk = require('chalk');
+const _ = require('lodash');
 
 console.log(chalk.cyan('\nEXERCISE 3 LOADED\n'));
 
-const reverseString = (input) => {
-
-  // Your future job begins here ...
+const reverseString = (input, reversedString) => {
+  if (input.length <= 0) { 
+    return '';
+  } else {
+    return reverseString(input.substring(1)) + input.charAt(0);
+  }
 }
 
 module.exports = {
